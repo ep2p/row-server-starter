186c1722d832210fb737f81ebbfc165c1ae6843e
sepehr <sepehrgh01@gmail.com>
2020-05-14 12:42:42 +0430

    Initialized Project

---
+++ src/main/java/labs/psychogen/row/SpringbootRestOverWebsocketStarterApplication.java
@@ -0,0 +1,13 @@
+package labs.psychogen.row;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestOverWebsocketStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestOverWebsocketStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/labs/psychogen/row/SpringbootRestOverWebsocketStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package labs.psychogen.row;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestOverWebsocketStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
