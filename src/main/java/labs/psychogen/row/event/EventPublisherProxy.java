7b13a02c07fcdd2e649dbe03dca37ea3a37516ff
sepehr <sepehrgh01@gmail.com>
2020-07-05 14:19:43 +0430

    Fixed event publisher structure

--- src/main/java/labs/psychogen/row/event/EventPublisherProxy.java
+++ src/main/java/labs/psychogen/row/event/EventPublisherProxy.java
@@ -1,13 +1,18 @@
 package labs.psychogen.row.event;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.reflect.AbstractInvocationHandler;
 import labs.psychogen.row.annotations.Publish;
+import labs.psychogen.row.domain.protocol.ResponseDto;
 import labs.psychogen.row.service.PublisherService;
+import labs.psychogen.row.utl.RequestResponseUtil;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.util.Assert;
 
 import java.lang.reflect.Method;
+
+import static labs.psychogen.row.config.Naming.SUBSCRIPTION_EVENT_HEADER_NAME;
 
 public class EventPublisherProxy extends AbstractInvocationHandler {
     private final ObjectMapper objectMapper;
@@ -31,7 +36,7 @@ public class EventPublisherProxy extends AbstractInvocationHandler {
         Assert.notNull(message, "Message can not be null");
 
         Publish publish = method.getDeclaringClass().getAnnotation(Publish.class);
-        String json = objectMapper.writeValueAsString(message);
+        String json = objectMapper.writeValueAsString(getResponse(message, publish.value()));
         if(publish.async()){
             publisherTaskExecutor.execute(new Runnable() {
                 @Override
@@ -44,5 +49,12 @@ public class EventPublisherProxy extends AbstractInvocationHandler {
         }
 
         return Void.TYPE;
+    }
+
+    private ResponseDto getResponse(Object message, String event) {
+        ResponseDto responseDto = new ResponseDto();
+        responseDto.setBody(message);
+        RequestResponseUtil.addHeader(SUBSCRIPTION_EVENT_HEADER_NAME, event, responseDto);
+        return responseDto;
     }
 }
