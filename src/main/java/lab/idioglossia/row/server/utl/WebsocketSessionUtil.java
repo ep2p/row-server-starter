59eb75b2d8f9b668cb637b87551fc7ec56962ede
sepehr-gh <sepehrgh01@gmail.com>
2020-12-30 12:20:54 +0330

    improved WebsocketSessionUtil to extract extra object

--- src/main/java/lab/idioglossia/row/server/utl/WebsocketSessionUtil.java
+++ src/main/java/lab/idioglossia/row/server/utl/WebsocketSessionUtil.java
@@ -14,4 +14,12 @@ public class WebsocketSessionUtil {
     public static Date getLastHeartbeat(WebSocketSession session){
         return (Date) session.getAttributes().get(Naming.IN_HEARTBEAT_ATTRIBUTE_NAME);
     }
+
+    public static <E> E getExtra(WebSocketSession session, Class<E> eClass){
+        return eClass.cast(session.getAttributes().get(Naming.EXTRA_ATTRIBUTE_NAME));
+    }
+
+    public static Object getExtra(WebSocketSession session){
+        return session.getAttributes().get(Naming.EXTRA_ATTRIBUTE_NAME);
+    }
 }
